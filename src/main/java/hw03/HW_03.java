57f72126133c5a9a3496f81bd32fd03bd8d9e371
Admin <aqilzeka99@gmail.com>
2020-02-11 15:01:27 +0400

    Completely Done!

--- src/main/java/hw03/HW_03.java
+++ src/main/java/hw03/HW_03.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class HW_03 {
     public static void main(String[] args) {
 
+
         Scanner scanner = new Scanner(System.in);
         String [][] arr = new String[7][2];
 
@@ -26,29 +27,69 @@ public class HW_03 {
 
 
         while (true){
-            System.out.print("Please, input the day of the week: ");
-            String s = scanner.nextLine();
-            String m = s.toLowerCase();
 
-            if (m.equals(arr[0][0].toLowerCase())){
-                System.out.println(String.format("Your tasks for %s: %s" , arr[0][0] , arr[0][1]));
-            } else if (m.equals(arr[1][0].toLowerCase())){
-                System.out.println(String.format("Your tasks for %s: %s" , arr[1][0] , arr[1][1]));
-            }else if (m.equals(arr[2][0].toLowerCase())){
-                System.out.println(String.format("Your tasks for %s: %s" , arr[2][0] , arr[2][1]));
-            }else if (m.equals(arr[3][0].toLowerCase())){
-                System.out.println(String.format("Your tasks for %s: %s" , arr[3][0] , arr[3][1]));
-            }else if (m.equals(arr[4][0].toLowerCase())){
-                System.out.println(String.format("Your tasks for %s: %s" , arr[4][0] , arr[4][1]));
-            }else if (m.equals(arr[5][0].toLowerCase())){
-                System.out.println(String.format("Your tasks for %s: %s" , arr[5][0] , arr[5][1]));
-            }else if (m.equals(arr[6][0].toLowerCase())){
-                System.out.println(String.format("Your tasks for %s: %s" , arr[6][0] , arr[6][1]));
-            }else if (m.equals("exit".toLowerCase())){
-                break;
-            } else {
-                System.out.println("Sorry, I don't understand you, please try again.");
-            }
+            System.out.print("Do you want to change yor task? Yes or No: ");
+            String select = new String();
+            select = scanner.nextLine();
+
+                if (select.toLowerCase().equals("yes")) {
+                    while (true){
+                    System.out.println("If you want to change task of week, just write: 'change WeekDay'sName', For exit write 'exit'. \nex: 'change Monday' ");
+                    String s = scanner.nextLine();
+                    String m = s.toLowerCase();
+                    if (m.equals(String.format("change %s", arr[0][0].toLowerCase()))) {
+                        System.out.println(String.format("Please, input new tasks for %s .", arr[0][0]));
+                        arr[0][1] = scanner.nextLine();
+                    } else if (m.equals(String.format("change %s", arr[1][0].toLowerCase()))) {
+                        System.out.println(String.format("Please, input new tasks for %s .", arr[1][0]));
+                        arr[1][1] = scanner.nextLine();
+                    } else if (m.equals(String.format("change %s", arr[2][0].toLowerCase()))) {
+                        System.out.println(String.format("Please, input new tasks for %s .", arr[2][0]));
+                        arr[2][1] = scanner.nextLine();
+                    } else if (m.equals(String.format("change %s", arr[3][0].toLowerCase()))) {
+                        System.out.println(String.format("Please, input new tasks for %s .", arr[3][0]));
+                        arr[3][1] = scanner.nextLine();
+                    } else if (m.equals(String.format("change %s", arr[4][0].toLowerCase()))) {
+                        System.out.println(String.format("Please, input new tasks for %s .", arr[4][0]));
+                        arr[4][1] = scanner.nextLine();
+                    } else if (m.equals(String.format("change %s", arr[5][0].toLowerCase()))) {
+                        System.out.println(String.format("Please, input new tasks for %s .", arr[5][0]));
+                        arr[5][1] = scanner.nextLine();
+                    } else if (m.equals(String.format("change %s", arr[6][0].toLowerCase()))) {
+                        System.out.println(String.format("Please, input new tasks for %s .", arr[6][0]));
+                        arr[6][1] = scanner.nextLine();
+                    } else if (m.equals("exit")) {
+                        break;
+                    }else {
+                        System.out.println("if you want to change task of week, just write 'change WeekDay'sName ' ");
+                    }
+                    }
+                } else if (select.toLowerCase().equals("no")) {
+                    System.out.println("Please, input the day of the week:");
+                    String s = scanner.nextLine();
+                    String m = s.toLowerCase();
+                    if (m.equals(arr[0][0].toLowerCase())) {
+                        System.out.println(String.format("Your tasks for %s: %s", arr[0][0], arr[0][1]));
+                    } else if (m.equals(arr[1][0].toLowerCase())) {
+                        System.out.println(String.format("Your tasks for %s: %s", arr[1][0], arr[1][1]));
+                    } else if (m.equals(arr[2][0].toLowerCase())) {
+                        System.out.println(String.format("Your tasks for %s: %s", arr[2][0], arr[2][1]));
+                    } else if (m.equals(arr[3][0].toLowerCase())) {
+                        System.out.println(String.format("Your tasks for %s: %s", arr[3][0], arr[3][1]));
+                    } else if (m.equals(arr[4][0].toLowerCase())) {
+                        System.out.println(String.format("Your tasks for %s: %s", arr[4][0], arr[4][1]));
+                    } else if (m.equals(arr[5][0].toLowerCase())) {
+                        System.out.println(String.format("Your tasks for %s: %s", arr[5][0], arr[5][1]));
+                    } else if (m.equals(arr[6][0].toLowerCase())) {
+                        System.out.println(String.format("Your tasks for %s: %s", arr[6][0], arr[6][1]));
+                    } else if (m.equals("exit".toLowerCase())) {
+                        break;
+                    } else {
+                        System.out.println("Sorry, I don't understand you, please try again.");
+                    }
+                } else {
+                    System.out.println("Please select only Yes or No");
+                }
 
         }
     }
